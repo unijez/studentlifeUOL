399a53e7f0d39a16b4a27da61138a8ca98fa46d1
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-07-10 13:11:48 +0100

    Added: ACF social media into the extendable menu

--- template-parts/footer-widgets.php
+++ template-parts/footer-widgets.php
@@ -17,24 +17,6 @@
 
 <div class="row">
 
-    <div class="column">
-
-      <?php if( function_exists('acf_add_local_field_group') )	:?>
-
-      <div class="widget-container widget_text">
-          <h2 class="widget-title">Contact Us</h2>
-
-          <div class="textwidget">
-            <p><?php echo get_field("address", "options"); ?></p>
-            <p><a href="tel:<?php echo get_field("telephone_number", "options")?>"><?php echo sanitize_text_field(get_field("telephone_number", "options")); ?></a></p>
-            <p><a href="mailto:<?php echo get_field("e-mail", "options")?>"><?php echo sanitize_text_field(get_field("e-mail", "options")); ?></a></p>
-          </div>
-
-      </div>	 <!--column-->
-
-
-    </div>	 <!--column-->
-
   <?php if( is_active_sidebar('footer-1') ):?>
 
     <div class="column">
@@ -51,8 +33,24 @@
 
   <div class="column socials">
       <!--Display Social Media List-->
-      <h2 class="widget-title">Social Media</h2><?php endif; ?>
+      <h2 class="widget-title">Follow Us</h2><?php endif; ?>
       <?php get_template_part( 'template-parts/site/social-media', 'icons' ); ?>
+
+  </div>	 <!--column-->
+
+  <div class="column">
+
+    <?php if( function_exists('acf_add_local_field_group') )	:?>
+
+    <div class="widget-container widget_text">
+        <h2 class="widget-title">Subscribe</h2>
+
+        <div class="textwidget">
+          
+        </div>
+
+    </div>	 <!--column-->
+
 
   </div>	 <!--column-->
 
