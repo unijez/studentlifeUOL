849a802b1f2204d30a0464c4f3c4bcd3639723ed
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-07-10 13:10:54 +0100

    Added and Edited: Front page featured listings

--- template-parts/featured-listing-front-page.php
+++ template-parts/featured-listing-front-page.php
@@ -10,15 +10,19 @@
 
 ?>
 
-<div class="latest-video-post" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div class="featured-post" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="item-text">
-		<div class="post-img">
-			<a href="<?php the_permalink(); ?>">
-				<?php default_image('large'); ?>
-			</a>
-		</div>
+    <h4 class="post-category"><?php
+    list_categories();
+    ?></h4>
 		<h3 class="post-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
 		<p><?php excerpt(20); ?></p>
+    <p><i class="post-auth"><?php the_author(); ?></i></p>
 	</div>
+  <div class="post-img">
+    <a href="<?php the_permalink(); ?>">
+      <?php default_image('large'); ?>
+    </a>
+  </div>
 </div>
