a1af9a157a23af69120f119bafdf6e1f2447fcba
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-10-12 13:18:47 +0100

    Removed: Author: and Tag: from archive titles

--- functions.php
+++ functions.php
@@ -168,13 +168,17 @@ add_action( 'widgets_init', 'studentlifeUOL_widgets_init' );
    echo $b_bio;
  }
 
- function prefix_category_title( $title ) {
-     if ( is_category() ) {
-         $title = single_cat_title( '', false );
-     }
-     return $title;
+ function prefix_archive_title( $title ) {
+		if ( is_category() ) {
+			$title = single_cat_title( '', false );
+		} elseif ( is_tag() ) {
+			$title = single_tag_title( '', false );
+		} elseif ( is_author() ) {
+			$title = '<span class="vcard">' . get_the_author() . '</span>' ;
+		}
+    return $title;
  }
- add_filter( 'get_the_archive_title', 'prefix_category_title' );
+ add_filter( 'get_the_archive_title', 'prefix_archive_title' );
 
  // Remove P Tags Around Images
  function filter_ptags_on_images($content){
