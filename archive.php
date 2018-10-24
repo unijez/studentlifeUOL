8d5968da2b4735df356dd0a8f7d5815950d4837f
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-10-24 13:40:21 +0100

    Added: Author Archive profile

--- archive.php
+++ archive.php
@@ -13,33 +13,34 @@
 
  get_header(); ?>
 
+ <?php
+ if(get_the_author_meta('ID')):
+   $author_archive = true;
+   $author_id = get_the_author_meta( 'ID' );
+   $curauth = get_user_by('ID', $author_id);
+   $avatar = get_avatar( get_the_author_meta('ID', $author_id) );
+
+
+   $user_nicename    = $curauth->user_nicename;
+   $display_name     = $curauth->display_name;
+   $user_description = $curauth->user_description;
+ ?>
+ <div class="author-archive-card">
+   <div class="column-spacings column column--1-of-3 column--medium-1-of-3 column--small-1-of-1 clear ">
+     <h2 class="home-section-title content-heading"><?php echo $user_nicename?></h2>
+     <p><?php echo $user_description; ?></p>
+   </div>
+   <?php $profile_pic = get_field( 'profile_picture', 'user_'. $author_id ); ?>
+   <img class="column column--2-of-3 column--medium-2-of-3 column--small-1-of-1 clear" src="<?php echo $profile_pic['url']?>" alt="profile picture">
+ </div>
+
+ <?php
+ endif;
+ ?>
+
 <main id="main" class="site-main site-module">
 
 	<div class="module-inner-wrap">
-          <?php
-          if(get_the_author_meta('ID')):
-            $author_archive = true;
-            $author_id = get_the_author_meta( 'ID' );
-          	$curauth = get_user_by('ID', $author_id);
-          	$avatar = get_avatar( get_the_author_meta('ID', $author_id) );
-
-
-          	$user_nicename    = $curauth->user_nicename;
-          	$display_name     = $curauth->display_name;
-          	$user_description = $curauth->user_description;
-          ?>
-          <div class="author-archive-card">
-            <div>
-              <h2 class="home-section-title content-heading"><?php echo $user_nicename?></h2>
-              <p><?php echo $user_description; ?></p>
-            </div>
-            <?php $profile_pic = get_field( 'profile_picture', 'user_'. $author_id ); ?>
-            <img src="<?php echo $profile_pic['url']?>" alt="profile picture">
-          </div>
-
-          <?php
-          endif;
-          ?>
 
 		 			<?php if ( have_posts() ) : ?>
 
