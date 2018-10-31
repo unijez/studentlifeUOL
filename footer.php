4cfd0e6c5caa19efc113b0ad9caf927183b55458
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-10-31 16:04:13 +0000

    Fixed: Footer stylings to make it more concurrent to the designs

--- footer.php
+++ footer.php
@@ -32,20 +32,24 @@
 
   <div class="footer-bottom site-module xl-b xl-t">
 
-		<div class="module-inner-wrap flex-controlled">
+		<div class="module-inner-wrap flex-controlled footer-alignment">
 
 
 			  	<div class="footer-meta">
-			  		<p class="credit">Designed by <b>Thomas Moore</b> (2nd year Interactive Design). Developed by <b>Brennan Ceballos Peña</b> (1st year Games Computing).</p>
-			  		<?php university_links(); ?>
 
-			  		 <p class="copyright">	&#169; <b>University of Lincoln.</b> All rights reserved</p>
+						<p><b>Student Life</b></p>
+			  		<p class="credit">Designed by Thomas Moore (2nd year Interactive Design). </p>
+						<p class="credit">Developed by Brennan Ceballos Peña (1st year Games Computing).</p>
+						<b>
+			  		<?php university_links(); ?>
+						</b>
+		  			<p class="copyright">	&#169; <b>University of Lincoln.</b> All rights reserved</p>
 
 			  </div> <!--footer-meta-->
 
 
 				<a class="site-footer__logo" href="http://www.lincoln.ac.uk/home/" rel="<?php bloginfo( 'name' ); ?>">
-					<?php get_template_part( 'template-parts/site/uol-logo', 'landscape' ); ?>
+					<?php get_template_part( 'template-parts/site/uol-logo', 'notext' ); ?>
 				</a>
 
 
