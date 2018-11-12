51d26fd7283ae40ef731f53af362a78eed33df1a
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-11-12 09:13:58 +0000

    Removed: Site title to avoid problems when linking in social media

--- header.php
+++ header.php
@@ -21,7 +21,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<title>Student Life at University of Lincoln</title>
 	<?php wp_head(); ?>
 </head>
 
